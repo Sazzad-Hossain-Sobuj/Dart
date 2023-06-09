e23964d6495587ec02e623b7f9ad29b744335c38
Sazzad Hossain <93114191+Sazzad-Hossain-Sobuj@users.noreply.github.com>
2023-06-09 21:17:34 +0600

    Add files via upload
    dart list

---
+++ dart_list.dart
@@ -0,0 +1,23 @@
+
+import 'dart:html_common';
+import 'dart:js_util';
+
+void main() {
+  var list1 = [10,20,30,40];
+  print(list1);
+  print(list1[3]);
+  print(list1[2]);
+  //fixed length list
+  /*syntax of fixed length list 
+  var listname = new list(size)
+  listname[index] = value*/  
+   var list2 = List(); 
+   list2[0] = 10;   
+   list2[1] = 11;   
+   list2[2] = 12;   
+   list2[3] = 13;  
+   list2[4] = 14;    
+   print(list2);   
+}  
+  
+
