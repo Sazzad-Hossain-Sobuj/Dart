acc96a8ddb46f58d122e29c2c7a9058da42fb303
Sazzad Hossain <93114191+Sazzad-Hossain-Sobuj@users.noreply.github.com>
2023-06-15 19:31:06 +0600

    Data_type

---
+++ data_type.dart
@@ -0,0 +1,10 @@
+import 'dart:ffi';
+ void main() {
+  print('dart data type: number,string,boolean,lists,maps,runes,symbol');
+  int marks = 85;
+  double pi = 3.1416;
+  String msg = 'Dart Data Type';
+  bool isvalid = true;
+  var list = [1,2,3,4];
+  var studentsdetails = {'name':'sazzad', 'age':25,'session':'191'};
+}
