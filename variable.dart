1f013075f9c0b8c6a1840a388194e7f551e55afc
Sazzad Hossain <93114191+Sazzad-Hossain-Sobuj@users.noreply.github.com>
2023-06-19 17:41:30 +0600

    variable

---
+++ variable.dart
@@ -0,0 +1,18 @@
+ void main() {
+  var fname = 'sazzad';
+  String lname = 'hossain';
+  var b = 25;
+  int a = 22;
+  int c,d;
+  c=50;d=100;
+  print(d/c);
+  print(fname);
+  print(lname);
+  print(fname + ' '+ lname +' ');
+  print(a+b);
+  print(a-b);
+  print(a*b);
+
+
+  
+}
